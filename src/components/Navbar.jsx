a5ca0aa4dc0f4c0a3b8cdd03f4505f0f472872b9
Sergio Rojas-Aguilar <128770839+Sergrojas29@users.noreply.github.com>
2023-09-26 18:24:08 -0400

    Change button to Link reactDOM

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import logo from '../assets/logoSra.svg'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    
+
     <header>
-        <div id="logoContainer">
-            <img src={logo} alt={logo} id="logo"></img>
-        </div>
-        <div id="btnContainer">
-            <a className="btnHeader" href='/'>HOME</a>
-            <a className="btnHeader" href='/project'>PROJECTS</a>
-            <a className="btnHeader" href='/aboutme' >ABOUT ME</a>
-            {/* <a className="btnHeader" href='/contactme'>CONTACT ME</a> */}
-        </div>
+      <div id="logoContainer">
+        <img src={logo} alt={logo} id="logo"></img>
+      </div>
+      <div id="btnContainer">
+        <Link className="btnHeader" to="/">
+          <a className="btnHeader" href='/'>HOME</a>
+        </Link>
+        <Link className="btnHeader" to="/project">
+          <a className="btnHeader" href='/'>PROJECTS</a>
+        </Link>
+        <Link className="btnHeader" to="/aboutme">
+          <a className="btnHeader" href='/' >ABOUT ME</a>
+        </Link>
+        {/* <a className="btnHeader" href='/contactme'>CONTACT ME</a> */}
+      </div>
     </header>
   );
 }
